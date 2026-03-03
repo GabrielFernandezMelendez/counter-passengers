bc9230bb092d0ce575a2d826084cd160057feae3
GabrielFernandezMelendez <gabriel.fernandezdeveloper@gmail.com>
2026-03-03 09:14:25 +0100

    refactorizacion de las funciones

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let count = 0;
-let countEl = document.getElementById("count-el");
+let contador = 0;
+let contadorTexto = document.getElementById("count-el");
 //esta es la variable para la funcion de guardado 
-let saveEl = document.getElementById("save-el");
+let Guardado = document.getElementById("save-el");
 
 function increment() {
-  count += 1;
-  console.log(count);
-  countEl.textContent = count;
+  contador += 1;
+  console.log("el contador tiene el valor de:"+contador);
+  contadorTexto.textContent = contador;
 }
 function save() {
-  let countStr = count + " - ";
-  saveEl.textContent += countStr;
-  countEl.textContent = 0;
-  count = 0;
+  let guardado = contador + " - ";
+  Guardado.textContent += guardado;
+  contadorTexto.textContent = 0;
+  contador = 0;
 }
